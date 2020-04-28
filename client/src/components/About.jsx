852b069ec5b4cf431652f2972b795f77f3cea431
kingdomlevel <nllmrrs@aol.com>
2020-04-28 16:18:14 +0100

    small styling changes to header + about pages

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from './Header';
+import Navigation from './Navigation';
 
 const About = () => {
     return (
         <>
+            <Header />
+            <Navigation />
             <p> Hello from the about page!</p>
         </>
     );

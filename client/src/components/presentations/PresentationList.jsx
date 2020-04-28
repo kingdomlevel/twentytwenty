075c5b115b7650568acffa8c687584cb81ef16e3
kingdomlevel <nllmrrs@aol.com>
2020-04-28 16:19:44 +0100

    tidied folder structure. moved presentation list to its own component for re-use in Users section

---
+++ client/src/components/presentations/PresentationList.jsx
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react';
+import { store } from '../../store.js';
+import './PresentationList.css';
+
+const PresentationList = ({ presentations }) => {
+    const {dispatch} = useContext(store);
+    
+    const presClick = (presentation) => {
+        // dispatch selected presentation to global
+        dispatch({
+            type: "SET_PRESENTATION",
+            data: {
+                presentation: presentation
+            }
+        });
+    }
+
+    const list = presentations.map((presentation, index) => {
+        return (
+            <li
+                className="presentation-in-list"
+                key={index} 
+                onClick={() => presClick(presentation)}>
+
+                <h3>{presentation.title}</h3>
+                {presentation.user.username && <p> by <a className="user-name" href="/">{presentation.user.username}</a></p>}
+                
+                <p className="last-updated">last updated: {new Date(presentation.updated_at).toLocaleDateString()}</p>
+
+            </li>
+        )
+    });
+
+    return (
+        <ul id="presentation-list">
+            {list}
+        </ul>
+    )
+};
+
+export default PresentationList;

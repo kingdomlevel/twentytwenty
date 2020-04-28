d4c6832138d23dc9d4f2900d47f608f56c0c4c03
kingdomlevel <nllmrrs@aol.com>
2020-04-28 16:20:55 +0100

    moved routing to presentations container for dynamic present routes

--- client/src/containers/PresentationsContainer.jsx
+++ client/src/containers/PresentationsContainer.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import PresentationList from '../components/PresentationList';
+import PresentationList from '../components/presentations/PresentationList';
+import PrePlayDetails from '../components/presentations/PrePlayDetails';
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PresentContainer from "./PresentContainer";
 
 const PresentationsContainer = () => {
     const [presentations, setPresentations] = useState([]);
-    const [presentation, setPresentation] = useState(null);
 
     useState(() => {
+        console.log("fetching presentations...")
         fetch('http://localhost:1337/presentations')
             .then(res => res.json())
             .then((data) => {
@@ -13,10 +18,33 @@ const PresentationsContainer = () => {
             });
     });
 
+    if (presentations.length === 0) return "no users";
+
+    const findPresentationById = (id) => {
+        return presentations.find((presentation) => {
+            return presentation.id === parseInt(id);
+        })
+    }
+
     return (
+        <Router>
         <>
-            <PresentationList presentations={presentations} />
+            <Switch>
+                <Route exact path="/present/:id" render={(props) => {
+                    const id = props.match.params.id;
+                    const presentation = findPresentationById(id);
+                    return <PresentContainer presentation={presentation}/>
+                }}/>
+
+                <Route path="/">
+                    <Header />
+                    <Navigation />
+                    <PresentationList presentations={presentations} />
+                    <PrePlayDetails/>
+                </Route>
+            </Switch>
         </>
+        </Router>
     )
 };
 

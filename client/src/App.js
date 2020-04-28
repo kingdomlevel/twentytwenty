968d61fb920fe5cddee473e5f966b6b80e706ed4
kingdomlevel <nllmrrs@aol.com>
2020-04-28 16:17:08 +0100

    moved render of header and nav down render tree to accomodate full screen pres

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import PresentationsContainer from './containers/PresentationsContainer';
 import UsersContainer from './containers/UsersContainer';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
 import About from './components/About';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  // Link
-  // ,useRouteMatch,
-  // useParams
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -19,25 +10,14 @@ function App() {
     <div className="App">
     
       <Router>
-        
+        <>
 
-        <Switch>
-          <Route path="/about">
-            <Header />
-            <Navigation />
-            <About/>
-          </Route>
-          <Route path="/users">
-            <Header />
-            <Navigation />
-            <UsersContainer/>
-          </Route>
-          <Route path="/">
-            <Header />
-            <Navigation/>
-            <PresentationsContainer />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route path="/about" component={About}/>
+            <Route path="/users" component={UsersContainer}/>
+            <Route path="/" component={PresentationsContainer}/>
+          </Switch>
+        </>
       </Router>
     </div>
   );

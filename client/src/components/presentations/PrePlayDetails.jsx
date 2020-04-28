58149e408ad0d703712a60a165aa91b642560b6a
kingdomlevel <nllmrrs@aol.com>
2020-04-28 16:21:31 +0100

    added simple pre-play component for presentation details prior to launching a presentation

---
+++ client/src/components/presentations/PrePlayDetails.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import {Link} from "react-router-dom";
+import { store } from '../../store';
+import './PrePlayDetails.css';
+
+const PrePlayDetails = () => {
+    const { state, dispatch } = useContext(store);
+    
+    if (!state.selectedPresentation) return null;
+    
+    const pres = state.selectedPresentation;
+
+    const closePop = () => {
+        // clear selectedPresentation in global state
+        dispatch({
+            type: "SET_PRESENTATION",
+            data: {
+                presentation: null
+            }
+        });
+    }
+
+    return (
+        <div className="pre-play-details">
+            <h2>{pres.title}</h2>
+            {pres.user.username && (<h3>
+                a presentation by <a className="user-name" href="/">{pres.user.username}</a>
+            </h3>)}
+            <p> {pres.slides.length} slides 
+                ({pres.slides.length * 20} seconds)</p>
+            <p>created: 
+                {new Date(pres.created_at).toLocaleString()}
+            </p>
+            <p className="last-updated">last updated: 
+                {new Date(pres.updated_at).toLocaleString()}
+            </p>
+
+            <p className="start">
+                <Link to={`/present/${pres.id}`}>
+                    <span>b</span>egin <span>p</span>resenting
+                </Link>
+            </p>
+            
+            <p className="close-link" onClick={closePop}>x</p>
+        </div>
+    )
+}
+
+export default PrePlayDetails;
